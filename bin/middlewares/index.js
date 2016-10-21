fe7a55aa363713573db4dc638bd78fd18ff43d4e
John Guignard <john.guignard@gmail.com>
2016-10-21 16:38:30 +0200

    Add webpack basis middleware

--- bin/middlewares/index.js
+++ bin/middlewares/index.js
@@ -1,6 +1,14 @@
+const path = require('path');
+const config = require('config');
 const winston = require('winston');
 const compose = require('koa-compose');
+const webpackDevServer = require('koa-webpack-dev');
+
+var middlewares = [];
+var webpackConfig = path.join(config.structure.root, 'config', 'webpack');
 
 winston.debug('Initializing middlewares ...');
 
-module.exports = compose([]);
+middlewares.push(webpackDevServer({ config: webpackConfig }));
+
+module.exports = compose(middlewares);
