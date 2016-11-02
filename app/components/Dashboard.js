251dd2837577749d55f73465b5e19bb3f749a2d7
Juaniyyoo <john.guignard@gmail.com>
2016-11-02 22:27:21 +0100

    Update fake dashboard

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -25,7 +25,16 @@ export default class Dashboard extends React.Component {
           <div className='w50 inbl spaced'>
             <Card inheritedClass='w100 mdl-shadow--2dp'>
               <CardText inheritedClass='w100'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                  sed do eiusmod tempor incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit
+                  esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                  sint occaecat cupidatat non proident, sunt in culpa qui
+                  officia deserunt mollit anim id est laborum.
+                </p>
               </CardText>
             </Card>
           </div>
@@ -47,6 +56,6 @@ export default class Dashboard extends React.Component {
       <LayoutTabPanel id='scroll-tab-3'>
         <div className='page-content'>Page 3</div>
       </LayoutTabPanel>
-    </div>
+    </div>;
   }
 }
