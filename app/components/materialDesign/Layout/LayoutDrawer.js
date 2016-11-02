509a9492a5c3f7a5c9c74c229640b80652468551
Juaniyyoo <john.guignard@gmail.com>
2016-11-02 22:16:02 +0100

    Add title to md LayoutDrawer

--- app/components/materialDesign/Layout/LayoutDrawer.js
+++ app/components/materialDesign/Layout/LayoutDrawer.js
@@ -2,14 +2,21 @@ import React, { PropTypes } from 'react';
 
 export default class LayoutDrawer extends React.Component {
   static propTypes = {
-    inheritedClass:  PropTypes.string
+    inheritedClass:  PropTypes.string,
+    title:           PropTypes.any
   };
 
   render () {
+    let title;
     let componentClass = 'mdl-layout__drawer ';
     componentClass += this.props.inheritedClass || '';
-    
+
+    if( this.props.title ) {
+      title = <span className='mdl-layout-title'>{ this.props.title }</span>
+    }
+
     return <div className={ componentClass }>
+      { title }
       { this.props.children }
     </div>
   }
