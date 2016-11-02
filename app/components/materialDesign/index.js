0fb10d5ced833ce85256d06f9722e646b82d33fd
Juaniyyoo <john.guignard@gmail.com>
2016-11-02 22:47:39 +0100

    Add default materialDesign gateway

---
+++ app/components/materialDesign/index.js
@@ -0,0 +1,29 @@
+import {
+  Layout,
+  LayoutTab,
+  LayoutTabBar,
+  LayoutTabPanel,
+  LayoutHeader,
+  LayoutHeaderRow,
+  LayoutContent
+} from './Layout';
+import Card from './Card';
+import CardText from './CardText';
+import TextField from './TextField';
+import Navigation from './Navigation';
+import NavigationLink from './NavigationLink';
+
+export {
+  Layout,
+  LayoutTab,
+  LayoutTabBar,
+  LayoutTabPanel,
+  LayoutHeader,
+  LayoutHeaderRow,
+  LayoutContent,
+  Card,
+  CardText,
+  TextField,
+  Navigation,
+  NavigationLink
+};
