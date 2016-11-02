1e644aa8d297a7d50855c809aceba2338e565a70
Juaniyyoo <john.guignard@gmail.com>
2016-11-02 22:28:05 +0100

    Remove title from DrawerContent

--- app/components/DrawerContent.js
+++ app/components/DrawerContent.js
@@ -1,24 +1,19 @@
 import React, { PropTypes } from 'react';
 import {
-  LayoutTabPanel,
-  Card,
-  CardText,
-  TextField
+  Navigation,
+  NavigationLink
 } from './materialDesign';
 
-export default class Dashboard extends React.Component {
+export default class DrawerContent extends React.Component {
   static propTypes = {
   };
 
   render () {
-    return <div>
-      <span className='mdl-layout-title'>Title</span>
-      <Navigation>
-        <NavigationLink href=''>Link</NavigationLink>
-        <NavigationLink href=''>Link</NavigationLink>
-        <NavigationLink href=''>Link</NavigationLink>
-        <NavigationLink href=''>Link</NavigationLink>
-      </Navigation>
-    </div>
+    return <Navigation>
+      <NavigationLink href=''>Link</NavigationLink>
+      <NavigationLink href=''>Link</NavigationLink>
+      <NavigationLink href=''>Link</NavigationLink>
+      <NavigationLink href=''>Link</NavigationLink>
+    </Navigation>
   }
 }
