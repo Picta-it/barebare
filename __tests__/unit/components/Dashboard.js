5e04b1c56825bfc7e6a04b5fd0822f6e764f9922
Juaniyyoo <john.guignard@gmail.com>
2016-11-07 21:20:17 +0100

    Rename tested file

--- __tests__/unit/components/Dashboard.js
+++ __tests__/unit/components/Dashboard.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Dashboard from '../../../app/components/Dashboard.js';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 test('Link changes the class when hovered', () => {
-  const component = renderer.create(
+  const component = shallow(
     <Dashboard>Facebook</Dashboard>
   );
 
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(component.hasClass('pouet')).toEqual(true);
 });
